2224890986208987c0e80eb22a08ea3526e7b7f9
jakelanghel <jakelangheldev@gmail.com>
2024-05-13 17:34:35 -0400

    add styled components, colors, and fonts

---
+++ src/soc-proof/Soc-Proof.jsx
@@ -0,0 +1,7 @@
+import { StyledSocProof } from "./styled/SocProof.Styled";
+
+const SocProof = () => {
+  return <StyledSocProof>Page</StyledSocProof>;
+};
+
+export default SocProof;
---
+++ src/soc-proof/styled/SocProof.Styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledSocProof = styled.main`
+  width: 100%;
+`;
